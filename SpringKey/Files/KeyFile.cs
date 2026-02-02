39b2f89dc2166fed0aa1a982950ff188e6b45c35
yLIue <2138347243@qq.com>
2026-02-02 23:41:32 +0800

    feat(Files.KeyFile): init.打了个框架，没怎么写东西

---
+++ SpringKey/Files/KeyFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpringKey.Struct;
+
+namespace SpringKey.Files
+{
+    class KeyFile
+    {
+        private const string secureVersion = "sksecure_ver0.1";
+
+        public const string SecureVersion = secureVersion;
+
+        public string UserPath;
+
+        public Key ken;
+        public KeyFile(string _userPath)
+        {
+            UserPath = _userPath;
+        }
+
+        public void LoadFile(string _userPath, string _fileHash) {
+            
+        }
+
+    }
+
+}
