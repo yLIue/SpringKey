b1715b960ca1d16aa3db83def4c75ecff022033b
yLIue <2138347243@qq.com>
2026-02-02 23:33:32 +0800

    feat(Models.SkHash): init -> add.GetFileHash(获取文件的哈希)

---
+++ SpringKey/Models/SkHash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SpringKey.Models
+{
+    public struct SkHash
+    {
+        public static string GetFileHash(byte[] _bytes)
+        {
+            using var sha = SHA256.Create();
+            String hash = BitConverter.ToString(sha.ComputeHash(_bytes))
+                .Replace("-", " ").ToLower();
+            return hash;
+        }
+    }
+}
