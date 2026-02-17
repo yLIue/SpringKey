ebd4bc3e64ffc06d92561c69ee543a8f5ad46131
yLIue <2138347243@qq.com>
2026-02-17 15:05:03 +0800

    feat(StructKeyInfo): init.KeyInfo是key的基础信息,包含哈希,分组,标题等信息,用于前端显示的结构体

---
+++ SpringKey/Struct/KeyInfo.cs
@@ -0,0 +1,37 @@
+﻿using SpringKey.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpringKey.Struct
+{
+    internal class KeyInfo
+    {
+        public string Title { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string Hash { get; set; }
+        public string Group { get; set; }
+
+        private string Null = "Null";
+        public KeyInfo(KeyFile _key, string _hash, string _group)
+        {
+            Title = _key.Title;
+            Account = _key.Account;
+            Password = _key.Password;
+            Hash = _hash;
+            Group = _group;
+        }
+
+        public KeyInfo()
+        {
+            Title = Null;
+            Account = Null;
+            Password = Null;
+            Hash = Null;
+            Group = Null;
+        }
+    }
+}
