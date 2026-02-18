623c9296d3ce6dc98a53469c93deb9369563b389
yLIue <2138347243@qq.com>
2026-02-18 11:40:18 +0800

    feat(Files.LinkFile): init.增加了Link文件和对应的api

---
+++ SpringKey/Files/LinkFile.cs
@@ -0,0 +1,91 @@
+﻿using SpringKey.Models;
+using SpringKey.Test;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SpringKey.Files
+{
+    // 关于这个文件的的介绍
+    // 采用userKey构造,将userKey的哈希和userName和user的index文件的路径进行关联
+    // 提供更改userName的功能
+    // 可返回index文件
+    internal class LinkFile
+    {
+        private string RootPath;
+        private string DataPath;
+        private string UserHash;
+        private string FilePath;
+        public string UserName; 
+        public string UserPath = "";
+        private const string Default = "default";
+
+        public LinkFile(string _appPath, string _userKey, string _userName = Default) 
+        {
+            RootPath = Path.Combine(_appPath, "link");
+            if (!Directory.Exists(RootPath)) Directory.CreateDirectory(RootPath);
+            DataPath = Path.Combine(_appPath, "data");
+            if (!Directory.Exists(DataPath)) Directory.CreateDirectory(DataPath);
+
+            UserHash = SkHash.GetFileHash($"<SpringKey>{_userKey}</SpringKey>");
+            FilePath = GetFilePath(UserHash);
+            UserName = _userName;
+
+            if (!File.Exists(FilePath))
+                Init();
+            else
+                Load();
+        }
+
+        private void Init()
+        {
+            if(UserName == Default)
+                UserName = AutoUserName();
+            UserPath = Path.Combine(DataPath, UserName);
+            if (!Directory.Exists(UserPath)) Directory.CreateDirectory(UserPath);
+            Updata();
+        }
+
+        private void Load()
+        {
+            string[] data = File.ReadAllLines(FilePath, Encoding.UTF8)[0].Split(' ');
+            UserName = data[0];
+            UserPath = data[1];
+        }
+
+        #region utils
+        private string GetFilePath(string _userHash)
+        {
+            return Path.Combine(RootPath, _userHash + ".sklink");
+        }
+
+        private string AutoUserName()
+        {
+            string[] names =
+            {
+                "杨间",
+                "姜小白",
+                "瑾旦",
+                "杨月",
+                "沐雪"
+            };
+            Random random = new Random();
+            int index = random.Next(names.Length);
+            return names[index];
+        }
+
+        private void Updata()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(UserName);
+            sb.Append(" ");
+            sb.Append(UserPath);
+            File.WriteAllText(FilePath, sb.ToString(), Encoding.UTF8);
+        }
+        #endregion
+    }
+}
