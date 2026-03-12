5558fefdce2df56070bd962db3b8e6d2f41f4d3a
yLIue <2138347243@qq.com>
2026-03-13 00:29:05 +0800

    feat(ViewModel.MainViewModel) init->简单写了下Main窗口里的Promp的默认值

---
+++ SpringKey/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SpringKey.MVVM;
+
+namespace SpringKey.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        private const string DefaultPrompt = "这里是提示语句";
+        private string prompt = DefaultPrompt;
+        public string Prompt
+        {
+            get => prompt;
+            set => SetProperty(ref prompt, value);
+        }
+    }
+}
