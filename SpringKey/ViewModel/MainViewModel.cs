91d59808fa652e4f9e3c6cfe2fe3d6d46724099f
yLIue <2138347243@qq.com>
2026-03-21 11:47:35 +0800

    feat(Main-ViewModel): 增加了登录框和用户界面框，实现了提示词的异步，实现了登录的功能(没有绑定后端数据，写死的)

--- SpringKey/ViewModel/MainViewModel.cs
+++ SpringKey/ViewModel/MainViewModel.cs
@@ -1,21 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using SpringKey.Files;
 using SpringKey.MVVM;
 
 namespace SpringKey.ViewModel
 {
     class MainViewModel : ViewModelBase
     {
+        #region Definition
+        public ICommand KeyEnterCommand { get; }
+
         private const string DefaultPrompt = "这里是提示语句";
-        private string prompt = DefaultPrompt;
+
+        private CancellationTokenSource? _cts;
+
+        private LinkFile? userLink;
+
+        private string appPath = AppDomain.CurrentDomain.BaseDirectory;
+
+        #endregion
+
+        #region MVVMDefinition
+        private Visibility _userVisibility = Visibility.Hidden;
+
+        public Visibility UserVisibility
+        {
+            get => _userVisibility;
+            set => SetProperty(ref _userVisibility, value);
+        }
+
+
+        private Visibility _loginVisibility = Visibility.Visible;
+
+        public Visibility LoginVisibility
+        {
+            get => _loginVisibility;
+            set => SetProperty(ref _loginVisibility, value);
+        }
+
+
+        private string _userKey = "";
+
+        public string UserKey
+        {
+            get => _userKey;
+            set => SetProperty(ref _userKey, value);
+        }
+
+        private string _prompt = DefaultPrompt;
         public string Prompt
         {
-            get => prompt;
-            set => SetProperty(ref prompt, value);
+            get => _prompt;
+            set => SetProperty(ref _prompt, value);
         }
+        #endregion
+
+        public MainViewModel()
+        {
+            KeyEnterCommand = new RelayCommand(KeyEnter);
+            appPath = Path.Combine(appPath, ".test");
+        }
+
+        private void KeyEnter()
+        {
+            if (UserKey == "")
+            {
+                _ = PromptChange($"EnterInputKey: key为空");
+                return;
+            }
+
+            userLink = new LinkFile(appPath, UserKey);
+
+            LoginVisibilityChange();
+            _ = PromptChange($"EnterInputKey: {UserKey},Hidden");
+        }
+
+        #region utils
+        private async Task PromptChange(string prompt)
+        {
+            _cts?.Cancel();
+            _cts?.Dispose();
+            _cts = new CancellationTokenSource();
+            Prompt = prompt;
+
+            try
+            {
+                await Task.Delay(5000, _cts.Token);
+                Prompt = DefaultPrompt;
+            }
+            catch (TaskCanceledException)
+            {
+            }
+        }
+
+        private void LoginVisibilityChange()
+        {
+            if (LoginVisibility == Visibility.Visible)
+            {
+                LoginVisibility = Visibility.Hidden;
+                UserVisibility = Visibility.Visible;
+
+            }
+            else
+            {
+                LoginVisibility = Visibility.Visible;
+                UserVisibility = Visibility.Hidden;
+            }
+        }
+        #endregion
     }
 }
