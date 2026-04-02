f6069ebe1b2523c4888d7c597c5cf756ec3e7766
yLIue <2138347243@qq.com>
2026-04-02 11:22:49 +0800

    feat(Services): 添加了IPromptService接口和实现

---
+++ SpringKey/Services/PromptService.cs
@@ -0,0 +1,11 @@
+﻿namespace SpringKey.Services;
+
+public class PromptService : IPromptService
+{
+    public event Action<string>? PromptRequested;
+    
+    public void Show(string message)
+    {
+        PromptRequested?.Invoke(message);
+    }
+}
