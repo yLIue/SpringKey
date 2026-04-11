0144efe5e05e42e944c2899f7674b111c763c2ec
yLIue <2138347243@qq.com>
2026-04-11 09:39:52 +0800

    feat(Struct.AddKeyParameter): init -> 管理AddFileView的传参

---
+++ SpringKey/Struct/AddKeyParameter.cs
@@ -0,0 +1,19 @@
+﻿namespace SpringKey.Struct;
+using Files;
+
+public class AddKeyParameter
+{
+    public IndexFile Index;
+    public string Group;
+
+    public AddKeyParameter(IndexFile index, string group)
+    {
+        Index = index;
+        Group = group;
+    }
+
+    public AddKeyParameter()
+    {
+        
+    }
+}
