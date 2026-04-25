9dc70f84729d5ebff9f8f52f7c6760f1d317634e
yLIue <2138347243@qq.com>
2026-04-25 10:29:58 +0800

    feat(Vm.KeyItemViewModel): init -> 密码项点击的ViewModel,根据KeyInfo构造

---
+++ SpringKey/ViewModel/KeyItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using SpringKey.MVVM;
+using SpringKey.Struct;
+using SpringKey.Files;
+
+namespace SpringKey.ViewModel;
+
+public class KeyItemViewModel(KeyInfo model) : ViewModelBase
+{
+    private readonly KeyInfo _model = model;
+    public string Title => _model.Title;
+    public string Account => _model.Account;
+    public string Password => _model.Password;
+    public string Hash => _model.Hash;
+    public string Group => _model.Group;
+
+    private string _copyText = "copy";
+    public string CopyText
+    {
+        get => _copyText;
+        set => SetProperty(ref _copyText, value);
+    }
+    
+    private Visibility _showVisibility = Visibility.Collapsed;
+    public Visibility ShowVisibility
+    {
+        get => _showVisibility;
+        set => SetProperty(ref _showVisibility, value);
+    }
+    
+    public KeyItemViewModel(KeyFile key, string hash, string group) 
+        : this(new KeyInfo(key, hash, group))
+    {
+    }
+}
